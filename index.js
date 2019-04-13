8074496aacf6f39421efdc6de76d2b84450ef3b9
Melissa Real <melissa.real@hotmail.com>
2019-04-13 14:55:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,43 @@ class Driver {
     constructor(name) {
         this.id = ++driverId;
         this.name = name;
-
-
     }
 
     trips() {
-        this.trips.count();
+      return this.trips.count();
     }
+
+    passengers(){
+      return this.passengers.count();
+    }
+    
+}
+
+class Passenger {
+
+  constructor(name) {
+      this.id = ++passengerId;
+      this.name = name;
+  }
+
+  
+}
+
+
+class Trip {
+  constructor(driverId, passengerId) {
+      this.id = ++tripId;
+      this.driverId = driverId;
+      this.passengerId = passengerId;
+  }
+
+  driver(){
+    dri = Driver.find(driverId);
+    return dri.name;
+  }
+  passenger(){
+    pass = Passenger.find(passengerId);
+    return pass.name;
+  }
+  
 }
