ef3573604959611eebf5b3e911ce7a25caeeb20b
Melissa Real <melissa.real@hotmail.com>
2019-04-13 14:59:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
   let driverId = 0;
   let passengerId = 0;
   let tripId = 0;
+  let store = { drivers: [], passengers: [], trips: [] };
 class Driver {
 
     constructor(name) {
         this.id = ++driverId;
         this.name = name;
+        store.drivers.push(this);
     }
 
     trips() {
@@ -23,6 +25,7 @@ class Passenger {
   constructor(name) {
       this.id = ++passengerId;
       this.name = name;
+      store.passengers.push(this);
   }
 
 
